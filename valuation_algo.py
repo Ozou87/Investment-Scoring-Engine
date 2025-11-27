30c9fde3c4e6bf2d47c1b59af25c3c93ddbe737e
Ozou87 <oefraty@gmail.com>
2025-11-27 19:39:49 +0200

    v2 algo

--- valuation_algo.py
+++ valuation_algo.py
@@ -1,1 +1,21 @@
+PE_INVALID = float('nan')
+PE_RATIO_THRESHOLDS = [
+    (0.6,95),
+    (0.85,80),
+    (1.15,50),
+    (1.5,30),
+    (2.0,15),
+                    ]
+PE_RATIO_DEFAULT = 0
 
+F_PE_INVALID = float('nan')
+F_PE_RATIO = [
+    (0.60,95),
+    (0.80,90),
+    (0.90,75),
+    (1.10,55),
+    (1.30,40),
+    (1.60,25),
+    (2.00,15),
+            ]
+F_PE_DEFAULT = 5
