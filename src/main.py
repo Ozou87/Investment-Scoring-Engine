1a464b2bd5e33654d63dbd27b704ab2b33a0dcd4
Ozou87 <oefraty@gmail.com>
2025-11-30 21:02:59 +0200

    reorginize

--- src/main.py
+++
@@ -1,9 +0,0 @@
-
-# def main():
-#     print("Project started successfully!")
-
-# if __name__ == "__main__":
-#     main()
-
-
-
--- valuation_algo.py
+++ valuation_algo.py
@@ -55,7 +55,7 @@ PFCF_RATIO_THRESHOLDS = [
                         ]
 PFCF_RATIO_DEFAULT = 5   
 
-#generic function that will find score and send it to specific function
+#generic function that will find score and will send it to be stored in " _score"
 def score_by_thresholds(stock_value: float, sector_value: float, thresholds, default_score) -> float:
     
     #protecting from dividing by - 0
@@ -70,12 +70,31 @@ def score_by_thresholds(stock_value: float, sector_value: float, thresholds, def
             return score
     return default_score
 
-stock_pe = float(input("pls enter stock P/E: "))
-sector_pe = float(input("pls enter sector P/E: "))
+stock_pe = float(input("Enter stock P/E: "))
+sector_pe = float(input("Enter sector P/E: "))
 
 stock_fpe = float(input("Enter stock Forward P/E: "))
 sector_fpe = float(input("Enter sector Forward P/E: "))
 
-result_1 = score_by_thresholds(stock_pe,sector_pe, PE_RATIO_THRESHOLDS, PE_RATIO_DEFAULT)
-print(f"this is the score for this data: {result_1}")
+stock_ps = float(input("Enter stock P/S: "))
+sector_ps = float(input("Enter sector P/S: "))
+
+stock_evebitda = float(input("Enter stock EV/EBITDA: "))
+sector_evebitda = float(input("Enter sector EV/EBITDA: "))
+
+stock_pfcf = float(input("Enter stock Price/FCF: "))
+sector_pfcf = float(input("Enter sector Price/FCF: "))
+
+pe_score = score_by_thresholds(stock_pe,sector_pe, PE_RATIO_THRESHOLDS, PE_RATIO_DEFAULT)
+fpe_score = score_by_thresholds(stock_fpe, sector_fpe, F_PE_RATIO, F_PE_DEFAULT)
+ps_score = score_by_thresholds(stock_ps, sector_ps, PS_RATIO_THRESHOLDS, PS_RATIO_DEFAULT)
+evebitda_score = score_by_thresholds(stock_evebitda, sector_evebitda, EVEBITDA_RATIO_THRESHOLDS, EVEBITDA_RATIO_DEFAULT)
+pfcf_score = score_by_thresholds(stock_pfcf, sector_pfcf, PFCF_RATIO_THRESHOLDS, PFCF_RATIO_DEFAULT)
+
+print("--------- Valuation Scores ---------")
+print(f"P/E score: {pe_score}")
+print(f"Forward P/E score: {fpe_score}")
+print(f"P/S score: {ps_score}")
+print(f"EV/EBITDA score: {evebitda_score}")
+print(f"Price/FCF score: {pfcf_score}")
 
