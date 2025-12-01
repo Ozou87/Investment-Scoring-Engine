2df2ca8864af14a0529cae1d9f20df7e59896e23
Ozou87 <oefraty@gmail.com>
2025-12-01 21:37:05 +0200

    changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fundemental_module import calculate_fundamental_scores
 from valuation_module import calculate_valuation_scores
 
 #loop that make sure its float and system wont crash
-def get_float_correct_input(prompt):
+def get_float(prompt) -> float:
 
     while True:
         value = input(prompt)
@@ -14,15 +14,23 @@ def get_float_correct_input(prompt):
 
 
 #asking user for fundamentals inputs: (in the future will be API)
-revenue_growth = get_float_correct_input("Enter stock Revenue Growth %: ")
-operating_margin = get_float_correct_input("Enter stock Operating margin %: ")
-debt_to_equity = get_float_correct_input("Enter stock Debt to Equity ratio: ")
-fcf_margin = get_float_correct_input("Enter stock Free Cash Flow Margin %: ")
+revenue_growth = get_float("Enter stock Revenue Growth %: ")
+operating_margin = get_float("Enter stock Operating margin %: ")
+debt_to_equity = get_float("Enter stock Debt to Equity ratio: ")
+fcf_margin = get_float("Enter stock Free Cash Flow Margin %: ")
 
-#asking user for valuations inputs: (in the future will be API)
+#placholder for Valuation values
 
-result_1 = calculate_fundamental_scores(revenue_growth,operating_margin,debt_to_equity,fcf_margin)
-print(f"Score is: {result_1}")
+#starting the fundamental cycle in order to get data
+fundamental_output = calculate_fundamental_scores(revenue_growth,operating_margin,debt_to_equity,fcf_margin)
+print(f"Score is: {fundamental_output}")
+
+#valuation_output = 
+#print(valuation_output)
+
+
+
+
 
 
 
