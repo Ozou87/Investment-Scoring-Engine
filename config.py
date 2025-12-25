19cdaf16a816902bf0280f0463b0b3b862537448
Ozou87 <oefraty@gmail.com>
2025-12-25 21:42:55 +0200

    new

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 
 # file will help determine between differnet type of sector and help selidify weighted scores per sector.
 
+def fundamental_weight_by_sector(sector_name: str) -> dict:
+    pass
+
+def valuation_weight_by_sector(sector_name: str) -> dict:
+    pass
+
+def moat_weight_by_sector(sector_name: str) -> dict:
+    pass
+
+
