0313fb78c98eb111b04d127d6dcae46d2c299c5e
Ozou87 <oefraty@gmail.com>
2026-03-04 23:07:30 +0200

    fixing company name

--- company_provider.py
+++ company_provider.py
@@ -10,12 +10,17 @@ def fetch_company_profile_from_api(ticker) -> dict:
     -sector
     """
     clean_ticker = ticker.strip().upper()
+
     file_path_0 = f"data_reports/json_file_0_{clean_ticker}.json"
     with open(file_path_0, "r", encoding="utf-8") as f:
             file_0 = json.load(f)
+
+    file_path_6 = f"data_reports/json_file_6_{clean_ticker}.json"
+    with open(file_path_6, "r", encoding="utf-8") as f:
+            file_6 = json.load(f)
     
-    #Extract from API:
-    company_name = file_0['data']['quoteSummary']['result'][0]['assetProfile']['longBusinessSummary'] 
+    #Extracting from json files:
+    company_name = file_6['data'][1]['quoteSummary']['result'][0]['price']['longName']
     sector = file_0['data']['quoteSummary']['result'][0]['assetProfile']['sector']
 
     return {
